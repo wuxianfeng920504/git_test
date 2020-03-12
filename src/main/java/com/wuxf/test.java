63088708627f9b81bdf56812b4e1c91e18b7472a
wuxianfeng920504 <949998011@qq.com>
2020-03-12 23:42:56 +0800

    my first commit

---
+++ src/main/java/com/wuxf/test.java
@@ -0,0 +1,12 @@
+package com.wuxf;
+
+/**
+ * 创建者：wuxf
+ * 创建时间：Mar 12, 2020 11:26:00 PM
+ * 类说明
+ */
+public class test {
+	public static void main(String[] args) {
+		System.err.println("");
+	}
+}
